3d535f6b860708696f97988fe346bf4e4e2a4b4f
candidosales <candidosg@gmail.com>
2013-05-27 14:06:27 -0300

    create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([{ email: 'candidosg@gmail.com' }, { password: 'mirafloris123' }, { password_confirmation: 'mirafloris123' }])
+
